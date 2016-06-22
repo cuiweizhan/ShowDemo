7904f3e23aca60463a66fdefcceee2322f9781de
Vison <vison@visontekimacbook-pro.local>
2016-06-22 14:07:52 +0800

    Demo

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Vison on 16/6/22.
+//  Copyright © 2016年 Vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
